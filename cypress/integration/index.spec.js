76251bffd8a6b2ef5088ecc58001f7b5ca6cbe3e
Cristian Vargas <cvg97@misena.edu.co>
2021-03-25 18:17:25 -0500

    finished implementation testing

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -5,7 +5,12 @@ const login = () =>{
     cy.get("#user-name").type("standard_user");
     cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
-} 
+}
+
+const logout = () => {
+    cy.get("#react-burger-menu-btn").click();
+    cy.get('#logout_sidebar_link').click();
+}
 
 const addProducstoToCart = () => {
     cy.get(".inventory_item button").eq(0).click();
@@ -22,14 +27,15 @@ describe("SUIT TEST", () => {
     });
 
 
-    it.skip("should sort products by name Z - A", () => {
-        
+    it("should sort products by name Z - A", () => {
         cy.get(".product_sort_container").select("za");
+        cy.get(".inventory_item_name").first().should("have.text", "Test.allTheThings() T-Shirt (Red)");
     });
 
-    it.skip("should sort products by name A - Z", () => {
+    it("should sort products by name A - Z", () => {
        
         cy.get(".product_sort_container").select("az");
+        cy.get(".inventory_item_name").first().should("have.text", "Sauce Labs Backpack");
     });
 
     it("should add products to the cart", () => {
@@ -37,7 +43,7 @@ describe("SUIT TEST", () => {
         cy.get(".fa-layers-counter.shopping_cart_badge").should("have.text", "3");
     });
 
-    it.skip("should remove products to the cart", ()=>{
+    it("should remove products to the cart", ()=>{
         
         cy.get(":nth-child(4) > .pricebar > .btn_primary").click();
         cy.get(".fa-layers-counter.shopping_cart_badge").should("have.text", "1");
@@ -45,7 +51,7 @@ describe("SUIT TEST", () => {
         cy.get(".fa-layers-counter.shopping_cart_badge").should("have.length", "0");
     });
 
-    it.skip("shoud remove products to the cart part in other view", () => {
+    it("shoud remove products to the cart part in other view", () => {
         addProducstoToCart();
         cy.get(".fa-layers-counter.shopping_cart_badge").should("have.text", "3");
         cy.get(".svg-inline--fa").click();
@@ -58,5 +64,23 @@ describe("SUIT TEST", () => {
         cy.get(".fa-layers-counter.shopping_cart_badge").should("have.length", "0");
     });
 
+    it("should complete checkout", () => {
+        addProducstoToCart();
+        cy.get(".svg-inline--fa").click();
+        cy.get("a.btn_action").click();
+        cy.get('[data-test=firstName]').type("Juan");
+        cy.get('[data-test=lastName]').type("Rafael");
+        cy.get('[data-test=postalCode]').type("ABC34E4");
+        cy.get('.btn_primary').click();
+        cy.get(".summary_subtotal_label").should("have.text", "Item total: $55.97");
+        
+        // Thanks you page
+        cy.get('.btn_action').click();
+        cy.get('.complete-text').should("have.text", "Your order has been dispatched, and will arrive just as fast as the pony can get there!");
+    });
+
+    afterEach(() => {
+        logout();
+    });
 
 });
